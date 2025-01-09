4c02dd3e2b2d16c2be693a92b5b05d9d8af86640
April <aprilhemingway00@gmail.com>
2025-01-08 17:43:57 -0700

    Event form: repository work

--- api/queries/events_queries.py
+++ api/queries/events_queries.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel, HttpUrl
-from typing import Optional
+from typing import Optional, Union
 from datetime import datetime
+from queries.pool import pool
+from psycopg.rows import class_row
+
 
 class EventIn(BaseModel):
     name: str
@@ -8,3 +11,38 @@ class EventIn(BaseModel):
     address: str
     date_time: datetime
     picture_url: Optional[HttpUrl]
+
+class EventOut(BaseModel):
+    id: int
+    name: str
+    description: str
+    address: str
+    date_time: datetime
+    picture_url: Optional[HttpUrl]
+
+class EventRepository:
+    def create(self, event: EventIn) -> EventOut:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor(row_factory=class_row(EventOut)) as db:
+                    result = db.execute(
+                        """
+                        INSERT INTO events
+                            (name, description, address, date_time, picture_url)
+                            VALUES
+                                (%s, %s, %s, %s, %s)
+                            RETURNING *;
+                        """,
+                        [
+                            event.name,
+                            event.description,
+                            event.address,
+                            event.date_time,
+                            event.picture_url
+                        ]
+                    )
+                    event = db.fetchone()
+                    return event
+
+        except Exception as e:
+            return {"message": "Event creation failed"}
--- api/routers/events.py
+++ api/routers/events.py
@@ -1,9 +1,18 @@
-from fastapi import APIRouter
-from queries.events_queries import EventIn
+from fastapi import APIRouter, Depends, Response
+from typing import Union
+from queries.events_queries import (
+    EventIn,
+    EventRepository,
+    EventOut
+)
 
 router = APIRouter()
 
-@router.post("/api/events")
-def create_event(event: EventIn):
-    print('event', event.name)
-    return event
+@router.post("/api/events", response_model=Union [EventOut])
+def create_event(
+    event: EventIn,
+    response: Response,
+    repo: EventRepository = Depends()
+):
+    response.status_code = 400
+    return repo.create(event)
