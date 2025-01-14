ac58b1d805dabcb47f43aae50948f94c73cad047
Hayden Schreiber <hayden.ann.schreiber@gmail.com>
2025-01-14 13:53:46 -0700

    main page frontend but no styling

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -1,13 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
-
 import ErrorNotification from './components/ErrorNotification'
-import Construct from './components/Construct'
-
 import './App.css'
 
-// When using environment variables, you should do a check to see if
-// they are defined or not and throw an appropriate error message
 const API_HOST = import.meta.env.VITE_API_HOST
 
 if (!API_HOST) {
@@ -15,38 +10,15 @@ if (!API_HOST) {
 }
 
 function App() {
-    const [launchInfo, setLaunchInfo] = useState()
     const [error, setError] = useState(null)
 
-    useEffect(() => {
-        async function getData() {
-            let url = `${API_HOST}/api/launch-details`
-            console.log('fastapi url: ', url)
-            let response = await fetch(url)
-            let data = await response.json()
-
-            if (response.ok) {
-                if (!data.launch_details) {
-                    console.log('drat! no launch data')
-                    setError('No launch data')
-                    return
-                }
-                console.log('got launch data!')
-                setLaunchInfo(data.launch_details)
-            } else {
-                console.log('drat! something happened')
-                setError(data.message)
-            }
-        }
-        getData()
-    }, [])
-
     return (
-        <div className="App">
-            <header className="App-header">{/* <Nav /> */}</header>
-            <Outlet />
-            <ErrorNotification error={error} />
-            <Construct info={launchInfo} />
+        <div className="min-h-screen bg-gray-50">
+            <div className="container mx-auto px-4">
+                <header className="App-header">{/* <Nav /> */}</header>
+                <Outlet />
+                <ErrorNotification error={error} />
+            </div>
         </div>
     )
 }
