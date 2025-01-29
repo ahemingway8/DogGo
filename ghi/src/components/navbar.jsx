2d78112c2716e79553230fc13d9c625218344cad
April <aprilhemingway00@gmail.com>
2025-01-29 16:15:14 -0700

    actually changed navbar

--- ghi/src/components/navbar.jsx
+++ ghi/src/components/navbar.jsx
@@ -138,54 +138,67 @@ const Navbar = () => {
                                             Places
                                         </Link>
                                     </li>
-                                    <li className="relative">
-                                        <button
-                                            onClick={toggleSubMenu}
-                                            className="block w-full px-4 py-2 hover:bg-light-green flex items-center justify-between"
-                                        >
-                                            Events
-                                            <span className={`ml-2 transform transition-transform ${subMenuOpen ? 'rotate-180' : ''}`}>
-                                                 <svg
-                                                    className="h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    fill="none"
-                                                    stroke="currentColor"
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    strokeWidth="2"
-                                                    viewBox="0 0 24 24"
-                                                    aria-hidden="true"
-                                                >
-                                                    <path d="M6 9l6 6 6-6"></path>
-                                                </svg>
-                                            </span>
-                                        </button>
+                                    {isLoggedIn && (
+                                        <li className="relative">
+                                            <button
+                                                onClick={toggleSubMenu}
+                                                className="block w-full px-4 py-2 hover:bg-light-green flex items-center justify-between"
+                                            >
+                                                Events
+                                                <span className={`ml-2 transform transition-transform ${subMenuOpen ? 'rotate-180' : ''}`}>
+                                                    <svg
+                                                        className="h-4 w-4"
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none"
+                                                        stroke="currentColor"
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        strokeWidth="2"
+                                                        viewBox="0 0 24 24"
+                                                        aria-hidden="true"
+                                                    >
+                                                        <path d="M6 9l6 6 6-6"></path>
+                                                    </svg>
+                                                </span>
+                                            </button>
 
-                                        {subMenuOpen && (
-                                            <div className="sub-menu-dropdown absolute top-full mt-2 w-48 bg-light-green text-white shadow-md rounded-lg">
-                                                <ul className="py-2">
-                                                    <li>
-                                                        <Link
-                                                            to="/events"
-                                                            className="block px-4 py-2 hover:bg-green"
-                                                            onClick={() => setMenuOpen(false)}
-                                                        >
-                                                            All Events
-                                                        </Link>
-                                                    </li>
-                                                    <li>
-                                                        <Link
-                                                            to="/my-events"
-                                                            className="block px-4 py-2 hover:bg-green"
-                                                            onClick={() => setMenuOpen(false)}
-                                                        >
-                                                            My Events
-                                                        </Link>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        )}
-                                    </li>
+                                            {subMenuOpen && (
+                                                <div className="sub-menu-dropdown absolute top-full mt-2 w-48 bg-light-green text-white shadow-md rounded-lg">
+                                                    <ul className="py-2">
+                                                        <li>
+                                                            <Link
+                                                                to="/events"
+                                                                className="block px-4 py-2 hover:bg-green"
+                                                                onClick={() => setMenuOpen(false)}
+                                                            >
+                                                                All Events
+                                                            </Link>
+                                                        </li>
+                                                        <li>
+                                                            <Link
+                                                                to="/my-events"
+                                                                className="block px-4 py-2 hover:bg-green"
+                                                                onClick={() => setMenuOpen(false)}
+                                                            >
+                                                                My Events
+                                                            </Link>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            )}
+                                        </li>
+                                    )}
+                                    {!isLoggedIn && (
+                                         <li>
+                                            <Link
+                                                to="/events"
+                                                className="block px-4 py-2 hover:bg-light-green"
+                                                onClick={() => setMenuOpen(false)}
+                                            >
+                                                Events
+                                            </Link>
+                                        </li>
+                                    )}
                                 </ul>
                             </div>
                         )}
