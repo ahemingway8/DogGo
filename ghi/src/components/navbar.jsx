b6f14540bfcf6e176c5198553739a87db59fd68b
Hayden Schreiber <hayden.ann.schreiber@gmail.com>
2025-01-31 13:13:15 -0700

    some more last minute styling

--- ghi/src/components/navbar.jsx
+++ ghi/src/components/navbar.jsx
@@ -30,8 +30,6 @@ const Navbar = () => {
         }, 100)
     }
 
-    const toggleSubMenu = () => setSubMenuOpen(prevState => !prevState)
-
     const handleLogout = async () => {
         try {
             await signout()
